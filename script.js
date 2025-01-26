e3b0309dd2786df6a8df909b513b3acb8f7169fc
rightbear <jjj17041113@gmail.com>
2025-01-26 10:37:24 +0800

    feat: add operator functions for calculator

---
+++ script.js
@@ -0,0 +1,35 @@
+function operate(num1, num2, operator) {
+  switch (operator) {
+    case '+':
+      return add(num1, num2);
+    case '-':
+      return substract(num1, num2);
+    case '*':
+      return multiply(num1, num2);
+    case '/':
+      return divide(num1, num2);
+    default:
+      return 'Invalid operator';
+  }
+}
+
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function substract(num1, num2){
+    return num1 - num2;
+}
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+function divide(num1, num2){
+    return num1 / num2;
+}
+
+console.log(operate(1,2,'+'));
+console.log(operate(4,2,'-'));
+console.log(operate(5,2,'*'));
+console.log(operate(10,2,'/'));
