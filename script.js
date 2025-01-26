58ea897b3586181425520da94bea176f76782971
rightbear <jjj17041113@gmail.com>
2025-01-26 11:26:01 +0800

    style: add buttons to the calculator

--- script.js
+++ script.js
@@ -28,8 +28,3 @@ function multiply(num1, num2){
 function divide(num1, num2){
     return num1 / num2;
 }
-
-console.log(operate(1,2,'+'));
-console.log(operate(4,2,'-'));
-console.log(operate(5,2,'*'));
-console.log(operate(10,2,'/'));
